28cb3056ad94dbce1d2ec7995d9c800c76f221a5
FaisalKhawaj <khawajfaisal981@gmail.com>
2025-02-27 16:06:55 +0500

    fix

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -10,23 +10,18 @@ const Form: React.FC = () => {
   const [email,setEmail]=useState("");
   const [password,setPassword]=useState("");
     const [formData, setFormData] = useState<FormData>({ email: "", password: "" });
-    const [errors, setErrors] = useState<Partial<FormData>>({});
   
-    const handleChange = useCallback((name: keyof FormData, value: string) => {
-      setFormData((prev) => ({ ...prev, [name]: value }));
-      console.log(formData)
-    },[]);
+  
   
     const handleSubmit = (e: React.FormEvent) => {
       e.preventDefault();
-      let newErrors: Partial<FormData> = {};
-      if (!formData.email) newErrors.email = "Email is required";
-      if (!formData.password) newErrors.password = "Password is required";
-      setErrors(newErrors);
+      // let newErrors: Partial<FormData> = {};
+      // if (!formData.email) newErrors.email = "Email is required";
+      // if (!formData.password) newErrors.password = "Password is required";
   
-      if (Object.keys(newErrors).length === 0) {
-        console.log("Form submitted", formData);
-      }
+      // if (Object.keys(newErrors).length === 0) {
+      //   console.log("Form submitted", formData);
+      // }
     };
   
     return (
